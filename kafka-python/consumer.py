f77ca504eb408a9cf6b3c9695654a4f30a7b590f
amansman77 <amansman77@gmail.com>
2021-08-03 04:43:40 +0000

    produce/consume 코드 정비

--- kafka-python/consumer.py
+++ kafka-python/consumer.py
@@ -2,4 +2,4 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer('foobar', group_id='foobar_group', bootstrap_servers='192.168.7.130:31090,192.168.7.130:31091,192.168.7.130:31092')
 for msg in consumer:
     # print ('[Receive data] ' + msg)
-    print (msg)
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
--- kafka-python/producer.py
+++ kafka-python/producer.py
@@ -1,6 +1,17 @@
 from kafka import KafkaProducer
 import json
 
+def on_send_success(record_metadata):
+    print('success send message')
+    print('topic: ' + record_metadata.topic)
+    print('partition: ' + str(record_metadata.partition))
+    print('offset: ' + str(record_metadata.offset))
+
+def on_send_error(excp):
+    print('fail send message')
+    print('I am an errback', exc_info=excp)
+    # handle exception
+
 producer = KafkaProducer(bootstrap_servers='192.168.7.130:31090,192.168.7.130:31091,192.168.7.130:31092'
                             # , value_serializer=lambda m: json.dumps(m).encode('ascii')
                         )
@@ -8,4 +19,6 @@ for _ in range(1):
     topic = 'alarm-sms'
     data = {'toName':'황호성', 'toPhoneNumber':'01086608024', 'message':'한번 더 테스트합니다.'}
     producer.send(topic, json.dumps(data).encode('ascii'))
-    print ('[Send data] Topic: ', topic, ', data: ', data)
+
+while True:
+    time.sleep(1)
