26d830d59d370c6ee36b58eda656fecd4b7963f1
amansman77 <amansman77@gmail.com>
2021-08-04 09:32:13 +0900

    예제가 정상동작하도록 수정

--- kafka-python/consumer.py
+++ kafka-python/consumer.py
@@ -1,5 +1,8 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('foobar', group_id='foobar_group', bootstrap_servers='192.168.7.130:31090,192.168.7.130:31091,192.168.7.130:31092')
-for msg in consumer:
+consumer = KafkaConsumer('foobar', group_id='foobar_group', bootstrap_servers=['localhost:31090']
+                            , auto_offset_reset='earliest', enable_auto_commit=False)
+for message in consumer:
     # print ('[Receive data] ' + msg)
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
--- kafka-python/producer.py
+++ kafka-python/producer.py
@@ -1,5 +1,5 @@
 from kafka import KafkaProducer
-import json
+import json, time
 
 def on_send_success(record_metadata):
     print('success send message')
@@ -12,13 +12,14 @@ def on_send_error(excp):
     print('I am an errback', exc_info=excp)
     # handle exception
 
-producer = KafkaProducer(bootstrap_servers='192.168.7.130:31090,192.168.7.130:31091,192.168.7.130:31092'
+producer = KafkaProducer(bootstrap_servers=['localhost:31090']
                             # , value_serializer=lambda m: json.dumps(m).encode('ascii')
                         )
 for _ in range(1):
-    topic = 'alarm-sms'
-    data = {'toName':'황호성', 'toPhoneNumber':'01086608024', 'message':'한번 더 테스트합니다.'}
-    producer.send(topic, json.dumps(data).encode('ascii'))
+    topic = 'foobar'
+    data = {'toName':'홍길동', 'toPhoneNumber':'01012344321', 'message':'한번 더 테스트합니다.'}
+    # producer.send(topic, json.dumps(data).encode('ascii'))
+    producer.send(topic, b'raw_bytes').add_callback(on_send_success).add_errback(on_send_error)
 
 while True:
     time.sleep(1)
